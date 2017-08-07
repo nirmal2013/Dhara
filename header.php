ee111c811939c112497c443f3d958e7c1eb2382a
nirmal2013 <nirmal2013@gmail.com>
2017-08-07 19:57:05 +0530

    1
    1

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 
 <head>
-
   <title>
     Dharasree
   </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel = "stylesheet"
      type = "text/css"
@@ -13,3 +14,16 @@
 </head>
 
 <body>
+
+  <!-- Navbar (sit on top) -->
+  <div class="w3-top">
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card-2">
+      <a href="#home" class="w3-bar-item w3-button"><b>Dharasree</b> Spa</a>
+      <!-- Float links to the right. Hide them on small screens -->
+      <div class="w3-right w3-hide-small">
+        <a href="#projects" class="w3-bar-item w3-button">Home</a>
+        <a href="#about" class="w3-bar-item w3-button">About</a>
+        <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+      </div>
+    </div>
+  </div>
